856219eaba3f8ea608589a18a0f7b63c635e9e08
filcuk <filcuk@users.noreply.github.com>
2026-01-05 19:43:35 +0000

    Fix encoding

--- src/decode.js
+++ src/decode.js
@@ -5,6 +5,7 @@ import * as base64 from "base64-js";
 
 export function decodeV1(rewrittenUrl) {
   let target = new URL(rewrittenUrl).searchParams.get("u");
+  target = target.replaceAll("*20", "%20");
   target = decodeURIComponent(target);
   target = decodeHTML(target);
   return target;
@@ -13,6 +14,7 @@ export function decodeV1(rewrittenUrl) {
 export function decodeV2(rewrittenUrl) {
   let target = new URL(rewrittenUrl).searchParams.get("u");
   target = target.replaceAll("-", "%").replaceAll("_", "/");
+  target = target.replaceAll("*20", "%20");
   target = decodeURIComponent(target);
   target = decodeHTML(target);
   return target;
